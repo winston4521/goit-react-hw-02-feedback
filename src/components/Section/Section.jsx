7a7173f3f55a7e040c0626cc36427d7492e7b524
Igor Nedoshytko <46812129+winston4521@users.noreply.github.com>
2022-10-26 10:53:05 +0200

    ss

--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import css from '<div className=""></div>/Section.css';
 
 export const Section = ({ title, children }) => {
   return (
-    <div className={css.feedbackFrom}>
-      <h2 className={css.feedbackFrom__title}>{title}</h2>
+    <div className="feedbackFrom">
+      <h2 className="feedbackFrom__title">{title}</h2>
       {children}
     </div>
   );
